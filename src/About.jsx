bd288badee242759e6e5a52360779bdfb36592c8
shivam rajvanshi <you@example.com>
2020-10-19 16:30:21 +0530

    first commit

---
+++ src/About.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import web from "../src/images/hero-img.png";
+import { NavLink } from "react-router-dom";
+
+import Common from "./Common";
+
+const About = () => {
+  return (
+    <>
+      <Common
+        name="Welcome to About page"
+        imgsrc={web}
+        visit="/contact"
+        btname="Contact Now"
+      />
+    </>
+  );
+};
+
+export default About;
